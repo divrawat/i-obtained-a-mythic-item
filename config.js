e9dfca4922bd8c84ce59fbe2a8f839cd3d475016
divrawat <divrawat2001@gmail>
2024-07-03 00:38:15 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,548 @@
+
+// export const DOMAIN = 'http://localhost:3000';
+export const DOMAIN = 'https://www.iobtainedamythicitem.in.net';
+
+
+export const MANGA_DESCRIPTION = `<a href="${DOMAIN}"><u>I Obtained a Mythic Item</u></a> is a tale of adventure and triumph, where the protagonist acquires a legendary artifact through perseverance and cunning. The story explores themes of ambition, discovery, and the consequences of wielding great power. It showcases the protagonist's journey towards achieving something extraordinary, amidst challenges and revelations that shape their destiny.`;
+
+export const MANGA_NAME = 'I Obtained a Mythic Item';
+export const MANGA_AUTHOR = 'Jung Seonyul';
+export const MANGA_RELEASE = '2024';
+export const MANGA_STATUS = 'Ongoing';
+export const MANGA_ARTIST = 'Jung Seonyul';
+export const MANGA_STUDIO = 'None';
+export const MANGA_GENRE = 'Action, Adventure, Drama, Fantasy';
+
+export const CHAPTER_PREFIX = 'i-obtained-a-mythic-item-chapter';
+export const IMAGE_PREFIX = 'images/i-obtained-a-mythic-item';
+export const NEXT_PREVIOUS_PREFIX = 'manga/i-obtained-a-mythic-item-chapter';
+export const URL_PREFIX = 'manga/i-obtained-a-mythic-item-chapter';
+
+export const AUTHOR_PAGE = `${DOMAIN}/i-obtained-a-mythic-item-team`;
+export const LOGO_URL = `${DOMAIN}/logo.webp`;
+export const COVER_IMG = `${DOMAIN}/cover.webp`;
+export const BEHIND_COVER_IMG = "https://yonglibrary.com/wp-content/uploads/2024/02/I-Obtained-A-Mythic-Item.jpg";
+
+
+// Navbar
+export const logo = <img src={`${LOGO_URL}`} alt="Logo" className="h-[75px] rounded-full w-[70px] mr-5 md:ml-0 ml-4 md:pb-2 md:my-0 my-1" />
+
+export const APP_NAME = 'I Obtained a Mythic Item';
+export const NavbarName = "I Obtained a Mythic Item";
+
+export const APP_DESCRIPTION = `Read ${MANGA_NAME} manga online at ${DOMAIN}. Aria, a determined adventurer whose quest leads her to discover a fabled artifact of immense power. As Aria navigates through perilous dungeons and confronts formidable foes, she unravels the mysteries surrounding the mythic item's origins and its profound significance. Alongside allies and facing adversaries, Aria's resolve is tested as she learns to wield the artifact responsibly, ultimately shaping her own legacy in the annals of legends.`;
+
+export const DISQUS_SHORTNAME = "my-cms-7";
+
+
+export const MANGA_SUMMARY = [
+    {
+        id: 1,
+        content: "In the sprawling epic I Obtained a Mythic Item, the protagonist, Aria, emerges as a beacon of determination and courage in a fantastical realm teeming with ancient secrets and untold dangers. Aria, a skilled adventurer with a keen intellect and a fearless spirit, embarks on a quest that promises not only riches but also the chance to leave an indelible mark on history."
+    },
+    {
+        id: 2,
+        content: "The journey begins with rumors whispered in taverns and written in cryptic texts: tales of a mythic item, said to possess unimaginable power and guarded by trials that test the bravest of souls. Aria, driven by both curiosity and a thirst for greatness, sets out to uncover the truth behind these legends. Her path is fraught with peril from the outset, as she traverses treacherous landscapes, battles fearsome creatures, and solves intricate puzzles that guard the artifact's secrets."
+    },
+    {
+        id: 3,
+        content: "Along the way, Aria forms alliances with fellow adventurers whose skills complement her own. Together, they navigate the labyrinthine dungeons and face challenges that push them to their limits. Each encounter reveals a piece of the artifact's history, its creators, and the consequences of its use in ages past."
+    },
+    {
+        id: 4,
+        content: "As Aria delves deeper into the mysteries surrounding the mythic item, she confronts moral dilemmas that challenge her understanding of power and responsibility. She grapples with the temptation to wield the artifact for personal gain versus using it to protect those she holds dear and to uphold justice in a world fraught with strife."
+    },
+    {
+        id: 5,
+        content: "Throughout her quest, Aria evolves not only as a formidable warrior but also as a leader and a scholar. Her journey is marked by moments of triumph and setbacks, each contributing to her growth and shaping her into a figure of legend in her own right."
+    },
+    {
+        id: 6,
+        content: "Ultimately, when Aria finally lays her hands on the mythic item, the culmination of her journey is a testament to her resilience and unwavering resolve. With the artifact in her possession, she must confront the ultimate challenge: to harness its power wisely and to ensure that its legacy is one of hope and redemption rather than destruction."
+    },
+    {
+        id: 7,
+        content: "I Obtained a Mythic Item is more than a tale of adventure—it is a narrative that explores the complexities of human ambition, the allure of power, and the enduring quest for meaning and identity. Aria's story resonates because it reflects universal truths about the human condition, offering readers a glimpse into a world where bravery and wisdom collide in the pursuit of greatness."
+    },
+];
+
+
+
+
+export const navLinks = [
+    { text: 'Home', href: `${DOMAIN}` },
+    { text: 'DMCA', href: `${DOMAIN}/dmca` },
+    { text: 'Terms & Conditions', href: `${DOMAIN}/terms-and-conditions` },
+];
+
+export const FooterLinks = [
+    { text: 'About', href: `${DOMAIN}/about` },
+    { text: 'Contact', href: `${DOMAIN}/contact` },
+    { text: 'Disclaimer', href: `${DOMAIN}/disclaimer` },
+    { text: 'Privacy Policy', href: `${DOMAIN}/privacy-policy` },
+    { text: 'Terms & Conditions', href: `${DOMAIN}/terms-and-conditions` }
+];
+
+
+
+export const RelatedMangaLinks = [
+    /*
+    {
+        imageUrl: `${DOMAIN}/related/frieran.webp`,
+        title: 'Frieran Manga',
+        link: 'https://www.frierenmanga.in.net',
+    },
+    */
+
+];
+
+
+
+
+
+export const chaptersData = [
+    {
+        "chapterNumber": "0",
+        "numImages": 19
+    },
+    {
+        "chapterNumber": "1",
+        "numImages": 26
+    },
+    {
+        "chapterNumber": "2",
+        "numImages": 28
+    },
+    {
+        "chapterNumber": "3",
+        "numImages": 33
+    },
+    {
+        "chapterNumber": "4",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "5",
+        "numImages": 21
+    },
+    {
+        "chapterNumber": "6",
+        "numImages": 16
+    },
+    {
+        "chapterNumber": "7",
+        "numImages": 15
+    },
+    {
+        "chapterNumber": "8",
+        "numImages": 17
+    },
+    {
+        "chapterNumber": "9",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "10",
+        "numImages": 18
+    },
+    {
+        "chapterNumber": "11",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "12",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "13",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "14",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "15",
+        "numImages": 22
+    },
+    {
+        "chapterNumber": "16",
+        "numImages": 26
+    },
+    {
+        "chapterNumber": "17",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "18",
+        "numImages": 16
+    },
+    {
+        "chapterNumber": "19",
+        "numImages": 19
+    },
+    {
+        "chapterNumber": "20",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "21",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "22",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "23",
+        "numImages": 15
+    },
+    {
+        "chapterNumber": "24",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "25",
+        "numImages": 15
+    },
+    {
+        "chapterNumber": "26",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "27",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "28",
+        "numImages": 15
+    },
+    {
+        "chapterNumber": "29",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "30",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "31",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "32",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "33",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "34",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "35",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "36",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "37",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "38",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "39",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "40",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "41",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "42",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "43",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "44",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "45",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "46",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "47",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "48",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "49",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "50",
+        "numImages": 7
+    },
+    {
+        "chapterNumber": "51",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "52",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "53",
+        "numImages": 8
+    },
+    {
+        "chapterNumber": "54",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "55",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "56",
+        "numImages": 8
+    },
+    {
+        "chapterNumber": "57",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "58",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "59",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "60",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "61",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "62",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "63",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "64",
+        "numImages": 28
+    },
+    {
+        "chapterNumber": "65",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "66",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "67",
+        "numImages": 26
+    },
+    {
+        "chapterNumber": "68",
+        "numImages": 24
+    },
+    {
+        "chapterNumber": "69",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "70",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "71",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "72",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "73",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "74",
+        "numImages": 26
+    },
+    {
+        "chapterNumber": "75",
+        "numImages": 15
+    },
+    {
+        "chapterNumber": "76",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "77",
+        "numImages": 27
+    },
+    {
+        "chapterNumber": "78",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "79",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "80",
+        "numImages": 24
+    },
+    {
+        "chapterNumber": "81",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "82",
+        "numImages": 23
+    },
+    {
+        "chapterNumber": "83",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "84",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "85",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "86",
+        "numImages": 22
+    },
+    {
+        "chapterNumber": "87",
+        "numImages": 17
+    },
+    {
+        "chapterNumber": "88",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "89",
+        "numImages": 34
+    },
+    {
+        "chapterNumber": "90",
+        "numImages": 5
+    },
+    {
+        "chapterNumber": "90a",
+        "numImages": 4
+    },
+    {
+        "chapterNumber": "90b",
+        "numImages": 5
+    },
+    {
+        "chapterNumber": "90c",
+        "numImages": 4
+    },
+    {
+        "chapterNumber": "91",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "92",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "93",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "94",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "95",
+        "numImages": 30
+    },
+    {
+        "chapterNumber": "96",
+        "numImages": 23
+    },
+    {
+        "chapterNumber": "97",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "98",
+        "numImages": 76
+    },
+    {
+        "chapterNumber": "99",
+        "numImages": 26
+    },
+    {
+        "chapterNumber": "100",
+        "numImages": 24
+    },
+    {
+        "chapterNumber": "101",
+        "numImages": 15
+    },
+    {
+        "chapterNumber": "102",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "103",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "104",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "105",
+        "numImages": 15
+    },
+    {
+        "chapterNumber": "106",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "107",
+        "numImages": 12
+    }
+]
